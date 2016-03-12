b7ee3608c5f798b4a32806395ad39d985655a095
Michael Lobel <mglobel@gmail.com>
2016-03-12 12:35:33 -0600

    add css to upload scripts

--- dev_upload.sh
+++ dev_upload.sh
@@ -3,3 +3,4 @@
 aws s3 cp index.html s3://www.dev.knowthyshelf.com/index.html
 aws s3 cp build/bundle.js s3://www.dev.knowthyshelf.com/bundle.js
 aws s3 cp build/bundle.js.map s3://www.dev.knowthyshelf.com/bundle.js.map
+aws s3 cp public/css/base.css s3://www.dev.knowthyshelf.com/public/css/base.css
--- production_upload.sh
+++ production_upload.sh
@@ -2,3 +2,4 @@
 
 aws s3 cp build/index.html s3://www.knowthyshelf.com/index.html
 aws s3 cp build/bundle.js s3://www.knowthyshelf.com/bundle.js
+aws s3 cp public/css/base.css s3://www.knowthyshelf.com/public/css/base.css
